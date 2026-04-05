9043ca5963e51fc8661a9362b512ee977039c7ed
José Alberto Rocha Munguía <josealrocmun@outlook.com>
2026-04-04 19:02:47 -0600

    Diseño de interfaces de Sign-In y Sign-Up con navegación entre ambas

---
+++ Lu/Lu/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  Lu
+//
+//  Created by José Alberto Rocha Munguía on 04/04/26.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Lu/Lu/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  Lu
+//
+//  Created by José Alberto Rocha Munguía on 04/04/26.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
